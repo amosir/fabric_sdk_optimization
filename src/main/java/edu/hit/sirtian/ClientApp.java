465124d0b681aadf9964cdb4943478d2b5550777
Tiansir-wg <15872397013@163.com>
2020-09-25 08:36:19 +0800

    initial commit

--- src/main/java/edu/hit/sirtian/ClientApp.java
+++ src/main/java/edu/hit/sirtian/ClientApp.java
@@ -4,74 +4,119 @@ SPDX-License-Identifier: Apache-2.0
 
 package edu.hit.sirtian;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import io.netty.util.concurrent.RejectedExecutionHandlers;
-import org.hyperledger.fabric.gateway.Contract;
-import org.hyperledger.fabric.gateway.Gateway;
-import org.hyperledger.fabric.gateway.Network;
-import org.hyperledger.fabric.gateway.Wallet;
-import org.hyperledger.fabric.gateway.Wallets;
+import org.hyperledger.fabric.gateway.*;
+import org.hyperledger.fabric.sdk.Peer;
 
 public class ClientApp {
 
-	static {
-		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
+//	static {
+//		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
+//	}
+
+	private static AtomicInteger count = new AtomicInteger(0);
+
+	private static class FabcarThread implements Runnable{
+
+		private static Wallet wallet;
+
+		private static final Path networkConfigPath =
+				Paths.get("test-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
+
+		private CountDownLatch latch;
+
+		private int carNo;
+
+		static {
+			try {
+				wallet = Wallets.newFileSystemWallet(Paths.get("wallet"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public FabcarThread(CountDownLatch latch,int carNo) {
+			this.latch = latch;
+			this.carNo = carNo;
+		}
+
+		@Override
+		public void run() {
+			try{
+				Gateway.Builder builder = Gateway.createBuilder();
+				builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+
+				// create a gateway connection
+				try (Gateway gateway = builder.connect()) {
+
+					// get the network and contract
+					Network network = gateway.getNetwork("mychannel");
+					Contract contract = network.getContract("fabcar");
+
+					ArrayList<Peer> peerList = new ArrayList(network.getChannel().getPeers());
+					Collections.shuffle(peerList);
+
+					List<Peer> endorsers = new ArrayList<>(2);
+
+					for(Peer peer:peerList){
+						if(peer.getName().contains("org1")){
+							endorsers.add(peer);
+							break;
+						}
+					}
+
+					for(Peer peer:peerList){
+						if(peer.getName().contains("org2")){
+							endorsers.add(peer);
+							break;
+						}
+					}
+
+					Transaction transaction  = contract.createTransaction("createCar");
+
+					transaction.setEndorsingPeers(endorsers);
+
+					transaction.submit("CAR" + carNo, "VW" + carNo, "Polo", "Grey", "Mary");
+
+					byte[] result = contract.evaluateTransaction("queryCar", "CAR" + carNo);
+
+					System.out.println(new String(result));
+
+					count.getAndIncrement();
+				}
+			}catch (Exception e){
+				System.out.printf("Thread execution failed on : %s,caused by %s\n","CAR" + carNo,e.getMessage());
+			}finally {
+				latch.countDown();
+			}
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
-		// Load a file system based wallet for managing identities.
-		Path walletPath = Paths.get("wallet");
-		Wallet wallet = Wallets.newFileSystemWallet(walletPath);
-		// load a CCP
-		Path networkConfigPath = Paths.get("test-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
 
 
-		int taskCount = 1000;
-		ExecutorService executor = Executors.newFixedThreadPool(taskCount);
+		int taskCount = 3000;
+
+		ExecutorService executor = Executors.newCachedThreadPool();
 
 		CountDownLatch countDownLatch = new CountDownLatch(taskCount);
 
 		for(int i = 0;i < taskCount;i++){
-			executor.submit(()->{
-				try{
-					Gateway.Builder builder = Gateway.createBuilder();
-					builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
-
-					// create a gateway connection
-					try (Gateway gateway = builder.connect()) {
-
-						// get the network and contract
-						Network network = gateway.getNetwork("mychannel");
-						Contract contract = network.getContract("fabcar");
-
-						byte[] result;
-
-//						result = contract.evaluateTransaction("queryAllCars");
-//						System.out.println(new String(result));
-
-						contract.submitTransaction("createCar", "CAR10", "VW", "Polo", "Grey", "Mary");
-//
-						result = contract.evaluateTransaction("queryCar", "CAR10");
-						System.out.println(new String(result));
-//
-						contract.submitTransaction("changeCarOwner", "CAR10", "Archie");
-//
-						result = contract.evaluateTransaction("queryCar", "CAR10");
-						System.out.println(new String(result));
-					}
-				}catch (Exception e){
-					e.printStackTrace();
-				}finally {
-					countDownLatch.countDown();
-				}
-			});
+			executor.submit(new FabcarThread(countDownLatch, i));
+			Thread.sleep(10);
 		}
 		countDownLatch.await();
 		executor.shutdown();
-		System.out.println("++++++++++++finished++++++++++++");
+		System.out.printf("++++++++++++%d Threads finished++++++++++++\n",count.get());
 	}
 
 }
